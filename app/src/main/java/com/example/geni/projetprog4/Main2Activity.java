57cbc18570ad8b2f82a94f78c7cb3ecd5120eeb8
frizouw <genevieverollin@outlook.com>
2018-04-12 10:32:06 -0400

    Modification des id de la méthode booléenne onNavigationItemSelected dans Main2Activity

--- app/src/main/java/com/example/geni/projetprog4/Main2Activity.java
+++ app/src/main/java/com/example/geni/projetprog4/Main2Activity.java
@@ -1,5 +1,6 @@
 package com.example.geni.projetprog4;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -77,20 +78,23 @@ public class Main2Activity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
+
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_profile) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_mes_recettes) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_amies) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_calendrier) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_decouverte) {
+
+        } else if (id == R.id.nav_configuration) {
+
+        }else if (id == R.id.nav_deconnexion) {
 
         }
 
