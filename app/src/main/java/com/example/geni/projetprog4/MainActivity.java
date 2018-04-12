e533ff209f3c024bfa92fa0b7b376178050c1df1
Geni <genevieverollin@outlook.com>
2018-04-12 14:24:10 -0400

    Ajout de Main3Activity = Page d'inscription

--- app/src/main/java/com/example/geni/projetprog4/MainActivity.java
+++ app/src/main/java/com/example/geni/projetprog4/MainActivity.java
@@ -5,17 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     //PROPRIÉTÉS
     private Button btnConnexion;
+    private TextView txtInscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //LE BOUTON POUR SE CONNECTER
         btnConnexion = (Button) findViewById(R.id.btnConnecter);
         btnConnexion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,7 +27,18 @@ public class MainActivity extends AppCompatActivity {
                 Intent intentConnexion = new Intent(MainActivity.this, Main2Activity.class);
                 startActivity(intentConnexion);
             }
+
         });
 
+        //LE BOUTON POUR S'INSCRIRE
+        txtInscription = (TextView) findViewById(R.id.txtInscription);
+        txtInscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentInscription = new Intent(MainActivity.this, Main3Activity.class);
+                startActivity(intentInscription);
+            }
+        });
     }
 }
