79ba9a5d8703be42b12ac65031fa72d5da12051c
Geni <genevieverollin@outlook.com>
2018-04-11 22:02:07 -0400

    Ajout du Main2Activity = DrawerMenu

--- app/src/main/java/com/example/geni/projetprog4/MainActivity.java
+++ app/src/main/java/com/example/geni/projetprog4/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //PROPRIÉTÉS
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
