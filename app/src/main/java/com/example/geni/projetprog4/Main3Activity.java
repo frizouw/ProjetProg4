e7d091d38b68774e4833a1ecc706b1f318026512
Geni <genevieverollin@outlook.com>
2018-04-14 14:53:06 -0400

    Modification de l'interface activity_main3.xml

--- app/src/main/java/com/example/geni/projetprog4/Main3Activity.java
+++ app/src/main/java/com/example/geni/projetprog4/Main3Activity.java
@@ -2,8 +2,16 @@ package com.example.geni.projetprog4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Main3Activity extends AppCompatActivity {
+
+    //Propriétés
+    private ImageView avatar;                   //Représente l'ImageView de l'avatar
+    private Button btnChoisirImage;             //Représente le bouton qui aide l'utilisateur à choisir l'image
+    private boolean changementImage = false;    //Pour savoir si l'image a changée
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +21,10 @@ public class Main3Activity extends AppCompatActivity {
         setTitle("Inscription");
         //Activation du bouton précédent dans la barre d'action : la direction est programmé dans AndroidManifest.xml
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Initialisations des propriétés
+        avatar = (ImageView) findViewById(R.id.imgAvatar);
+        btnChoisirImage = (Button) findViewById(R.id.btnChoisirImage);
+
     }
 }
