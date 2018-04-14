b95ccb9411002faedef8670f167c561acba4e1f8
Geni <genevieverollin@outlook.com>
2018-04-14 17:55:41 -0400

    SharedPreferences

--- app/src/main/java/com/example/geni/projetprog4/MainActivity.java
+++ app/src/main/java/com/example/geni/projetprog4/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.geni.projetprog4;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
     //PROPRIÉTÉS
     private Button btnConnexion;
     private TextView txtInscription;
+    private EditText txtIdentifiant, txtMotDePasse;
+    private CheckBox checkSouvenir;
+    private SharedPreferences pref;
+    private static final String NOM_PREF = "sharedPrefs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +26,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("Cooking jar");
 
-        //LE BOUTON POUR SE CONNECTER
+        //INITIALISATION DES PROPRIÉTÉS/OBJETS
         btnConnexion = (Button) findViewById(R.id.btnConnecter);
+        txtInscription = (TextView) findViewById(R.id.txtInscription);
+        txtIdentifiant = (EditText) findViewById(R.id.txtIdentifiantConnexion);
+        txtMotDePasse = (EditText) findViewById(R.id.txtMotDePasseConnexion);
+        checkSouvenir = (CheckBox) findViewById(R.id.checkBoxMemoriser);
+        chargement();
+
+        //LE BOUTON POUR SE CONNECTER
         btnConnexion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intentConnexion = new Intent(MainActivity.this, Main2Activity.class);
-                startActivity(intentConnexion);
+                seConnecter();
             }
 
         });
 
         //LE BOUTON POUR S'INSCRIRE
-        txtInscription = (TextView) findViewById(R.id.txtInscription);
         txtInscription.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,5 +53,31 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intentInscription);
             }
         });
+
+    }
+
+    //LA MÉTHODE POUR SE CONNECTER
+    public void seConnecter()
+    {
+        Intent intentConnexion = new Intent(MainActivity.this, Main2Activity.class).putExtra("Identifiant", txtIdentifiant.getText().toString());
+        pref = getSharedPreferences(NOM_PREF, MODE_PRIVATE);
+        pref.edit().putString("Identifiant",(txtIdentifiant.getText().toString()))
+                .putString("MotDePasse",(txtMotDePasse.getText().toString()))
+                .putBoolean("Memoriser",(checkSouvenir.isChecked())).commit();
+        startActivity(intentConnexion);
+    }
+
+    //LA MÉTHODE DU CHARGEMENT DE L'ACTIVITÉ POUR LE SHAREDPREFERENCE
+    private void chargement()
+    {
+        pref = getSharedPreferences(NOM_PREF, MODE_PRIVATE);
+        if (pref.contains("Memoriser") && pref.getBoolean("Memoriser", false))
+        {
+            if(pref.contains("Identifiant"))
+                ((EditText)findViewById(R.id.txtIdentifiantConnexion)).setText(pref.getString("Identifiant", "Default"));
+            if(pref.contains("MotDePasse"))
+                ((EditText)findViewById(R.id.txtMotDePasseConnexion)).setText(String.valueOf(pref.getString("MotDePasse", "Default")));
+            ((CheckBox)findViewById(R.id.checkBoxMemoriser)).setChecked(true);
+        }
     }
 }
