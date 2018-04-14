10e38d28b11d49d408a4f39669924147643f6305
Claire <clairebun_3@hotmail.com>
2018-04-14 13:00:31 -0400

    Ajout dans la BD

--- app/src/main/java/com/example/geni/projetprog4/BD.java
+++ app/src/main/java/com/example/geni/projetprog4/BD.java
@@ -21,9 +21,13 @@ public class BD {
         try
         {
             //Table des utilisateurs
-            sql.execSQL("CREATE TABLE IF NOT EXISTS Utilisateurs(Identifiant VARCHAR(50) PRIMARY KEY NOT NULL, MotDePasse VARCHAR(50) NOT NULL, Pays VARCHAR(50), Courriel VARCHAR(50) NOT NULL, Pointage INTEGER)");
+            sql.execSQL("CREATE TABLE IF NOT EXISTS Utilisateurs(IdUtilisateur VARCHAR(50) PRIMARY KEY NOT NULL, MotDePasse VARCHAR(50) NOT NULL, Pays VARCHAR(50), Courriel VARCHAR(100) NOT NULL, Pointage INTEGER)");
             //Table des recettes
-            sql.execSQL("CREATE TABLE IF NOT EXISTS Recettes(NumeroRecette INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, NomRecette VARCHAR(50), PaysRecette VARCHAR(50), NiveauRecette INTEGER)");
+            sql.execSQL("CREATE TABLE IF NOT EXISTS Recettes(NumeroRecette INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, NomRecette VARCHAR(50), PaysRecette VARCHAR(50), NiveauRecette INTEGER, DureePrep INTEGER, DureeCuisson INTEGER, Ingredients VARCHAR(200), TypePlat VARCHAR(50), Calories INTEGER, Preparation VARCHAR(150))");
+            //Table des commentaires
+            sql.execSQL("CREATE TABLE IF NOT EXISTS Commentaire(NumeroCommentaire INTEGER PRIMARY KEY NOT NULL, Note INTEGER NOT NULL, FOREIGN KEY(IdUtilisateur VARCHAR(50))) REFERENCES Utilisateurs(IdUtilisateur), FOREIGN KEY(NumeroRecette INTEGER) REFERENCES Recettes(NumeroRecette) ");
+            //Table UtilisateurRecette
+            sql.execSQL("CREATE TABLE IF NOT EXISTS UtilisateurRecette()");
         }
         catch (Exception ex)
         {
@@ -42,6 +46,27 @@ public class BD {
         catch (Exception ex)
         {
             ex.printStackTrace();
+        }
+    }
+
+    //Insérer des recettes dans la base de données
+    public void insertRecette(String nomRecette, String paysRecette, Integer NiveauRecette, Integer dureePrep, Integer dureeCuisson, String Ingredients, String typePlat, Integer Calories, String Preparations){
+        try{
+            sql.execSQL(String.format("INSERT INTO Recette(NomRecette, PaysRecette, NiveauRecette, DureePrep, DureeCuisson, Ingredients, TypePlat, Calories, Preparation) VALUES ('%s', '%s', '%s', '%s','%s','%s','%s','%s','%s' )",nomRecette ,paysRecette , NiveauRecette, dureePrep,dureeCuisson, Ingredients,typePlat,Calories,Preparations));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //Insert des commentaires dans la table des commentaires
+    public void insertCommentaire()
+    {
+        try{
+
+        }
+        catch (Exception e){
+
         }
     }
 }
