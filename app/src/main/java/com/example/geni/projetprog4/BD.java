7222cc675dce9315f6f6099d842c6b004b01c987
Geni <genevieverollin@outlook.com>
2018-04-14 18:50:37 -0400

    Modification BD pour l'insertion d'un avatar

--- app/src/main/java/com/example/geni/projetprog4/BD.java
+++ app/src/main/java/com/example/geni/projetprog4/BD.java
@@ -21,7 +21,7 @@ public class BD {
         try
         {
             //Table des utilisateurs
-            sql.execSQL("CREATE TABLE IF NOT EXISTS Utilisateurs(IdUtilisateur VARCHAR(50) PRIMARY KEY NOT NULL, MotDePasse VARCHAR(50) NOT NULL, Pays VARCHAR(50), Courriel VARCHAR(100) NOT NULL, Pointage INTEGER)");
+            sql.execSQL("CREATE TABLE IF NOT EXISTS Utilisateurs(IdUtilisateur VARCHAR(50) PRIMARY KEY NOT NULL, MotDePasse VARCHAR(50) NOT NULL, Pays VARCHAR(50), Courriel VARCHAR(100) NOT NULL, NomImage VARCHAR(50), Image blob,  Pointage INTEGER)");
             //Table des recettes
             sql.execSQL("CREATE TABLE IF NOT EXISTS Recettes(NumeroRecette INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, NomRecette VARCHAR(50), PaysRecette VARCHAR(50), NiveauRecette INTEGER, DureePrep INTEGER, DureeCuisson INTEGER, Ingredients VARCHAR(200), TypePlat VARCHAR(50), Calories INTEGER, Preparation VARCHAR(150))");
             //Table des commentaires
@@ -37,11 +37,11 @@ public class BD {
     }
 
     //Insérer des utilisateurs dans la base de données de l'application
-    public void insererUtilisateurs(String Identifiant, String MotDePasse, String Pays, String Courriel)
+    public void insererUtilisateurs(String Identifiant, String MotDePasse, String Pays, String Courriel, String NomImage, byte[] Avatar)
     {
         try
         {
-            sql.execSQL(String.format("INSERT INTO Utilisateurs(Identifiant, MotDePasse, Pays, Courriel) VALUES ('%s', '%s', '%s', '%s', )", Identifiant, MotDePasse, Pays, Courriel));
+            sql.execSQL(String.format("INSERT INTO Utilisateurs(Identifiant, MotDePasse, Pays, Courriel, NomImage, Avatar) VALUES ('%s', '%s', '%s', '%s', )", Identifiant, MotDePasse, Pays, Courriel, NomImage, Avatar));
         }
         catch (Exception ex)
         {
--- app/src/main/java/com/example/geni/projetprog4/Main3Activity.java
+++ app/src/main/java/com/example/geni/projetprog4/Main3Activity.java
@@ -1,17 +1,24 @@
 package com.example.geni.projetprog4;
 
+import android.Manifest;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
 
 public class Main3Activity extends AppCompatActivity {
 
-    //Propriétés
+    //PROPRIÉTÉS
     private ImageView avatar;                   //Représente l'ImageView de l'avatar
     private Button btnChoisirImage;             //Représente le bouton qui aide l'utilisateur à choisir l'image
-    private boolean changementImage = false;    //Pour savoir si l'image a changée
+    private Button btnInscrire;                 //Représente le bouton pour s'inscrire
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +26,30 @@ public class Main3Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main3);
         //Titre de l'activité
         setTitle("Inscription");
-        //Activation du bouton précédent dans la barre d'action : la direction est programmé dans AndroidManifest.xml
+        //Activation du bouton précédent dans la barre d'action : la direction est programmée dans AndroidManifest.xml
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //Initialisations des propriétés
+        //INITIALISATIONS DES PROPRIÉTÉS
         avatar = (ImageView) findViewById(R.id.imgAvatar);
         btnChoisirImage = (Button) findViewById(R.id.btnChoisirImage);
+        btnInscrire = (Button) findViewById(R.id.btnInscription);
+
+        //ÉVÉNEMENTS
+        btnChoisirImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnInscrire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        });
 
     }
+
 }
