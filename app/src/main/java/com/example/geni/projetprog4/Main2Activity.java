2b01afadec582b8b6adfedf0ea4d9ad9b846286b
frizouw <genevieverollin@outlook.com>
2018-05-09 11:41:48 -0400

    gen, jmexcuse anthony, j'ai retiré un affaire ac main2 pus update UI

--- app/src/main/java/com/example/geni/projetprog4/Main2Activity.java
+++ app/src/main/java/com/example/geni/projetprog4/Main2Activity.java
@@ -124,12 +124,6 @@ public class Main2Activity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main2, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
