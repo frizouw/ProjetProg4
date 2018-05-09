b54d10a6e43b18f9d1ffd6bc1177a1651623e57c
frizouw <genevieverollin@outlook.com>
2018-05-09 08:58:30 -0400

    Yep c'est un commit de marde

--- app/src/main/java/com/example/geni/projetprog4/Decouvrir.java
+++ app/src/main/java/com/example/geni/projetprog4/Decouvrir.java
@@ -1,18 +1,21 @@
 package com.example.geni.projetprog4;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Decouvrir extends Fragment {
 
@@ -24,6 +27,7 @@ public class Decouvrir extends Fragment {
     private ListView listResultat;
     private Button btnRecherche;
     private Button btnAleatoire;
+    private ArrayList<String> recetteRechercher;
 
     @Nullable
     @Override
@@ -55,35 +59,59 @@ public class Decouvrir extends Fragment {
         adapterDifficulter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         spinnerDifficulter.setAdapter(adapterDifficulter);
 
-
-        btnRecherche.setOnClickListener(new View.OnClickListener() {
+        //Quand on appuie sur bouton Rechercher du mode découvrir
+        btnRecherche.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View v)
             {
-                ArrayList<String> recetteRechercher = new ArrayList<>();
+                recetteRechercher = new ArrayList<>();
 
-                Log.v("SHIT","coucou");
-
-                for (int position = 0; position  < Utils.LIST_RECETTES.size(); position++)
+                for (Recettes recette : Utils.LIST_RECETTES)
                 {
-                    if (Utils.LIST_RECETTES.get(position).getType() == spinnerRecette.getSelectedItem().toString() && Utils.LIST_RECETTES.get(position).getPays() == spinnerPays.getSelectedItem().toString() && Utils.LIST_RECETTES.get(position).getNiveau() == Integer.parseInt(spinnerDifficulter.getSelectedItem().toString()))
+                    if (recette.getType().equals(spinnerRecette.getSelectedItem().toString()) && recette.getPays().equals(spinnerPays.getSelectedItem().toString()) && recette.getNiveau() == Integer.parseInt(spinnerDifficulter.getSelectedItem().toString()))
                     {
-                        recetteRechercher.add(Utils.LIST_RECETTES.get(position).getNom());
-
-                        Log.v("SHIT",Utils.LIST_RECETTES.get(position).getNom());
+                        recetteRechercher.add(recette.getNom());
                     }
                 }
 
                 ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.liste_custom, recetteRechercher);
                 listResultat.setAdapter(adapter);
-
             }
         });
 
+        //Quand on appuie sur le bouton Aléatoire du mode découvrir (ce qui permet d'obtenir une recette aléatoire)
         btnAleatoire.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
+                recetteRechercher = new ArrayList<>();
+                Random aleatoire = new Random();
+                int recettePosition = aleatoire.nextInt(Utils.LIST_RECETTES.size());
+                recetteRechercher.add(Utils.LIST_RECETTES.get(recettePosition).getNom());
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.liste_custom, recetteRechercher);
+                listResultat.setAdapter(adapter);
+            }
+        });
 
+        //Quand on clique sur la recette dans la liste
+        listResultat.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                int recettePos = 0;
+                for (Recettes recette : Utils.LIST_RECETTES)
+                {
+                    if (recette.getNom().equals(parent.getItemAtPosition(position).toString()))
+                    {
+                        recettePos = Utils.LIST_RECETTES.indexOf(recette);
+                        break;
+                    }
+                }
+                Intent i = new  Intent(getActivity(), PageRecette.class);
+                i.putExtra("recette", recettePos);
+                Log.i("pos", String.valueOf(position));
+                startActivity(i);
             }
         });
         return vue;
